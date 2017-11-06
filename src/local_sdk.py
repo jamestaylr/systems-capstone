4d5c44c89ef9a6d6d27280b97ba0c4dfbb1c7b92
Christy Coghlan <ccogs@vt.edu>
2017-11-06 12:14:47 -0500

    Added new local sdk

--- src/local_sdk.py
+++ src/local_sdk.py
@@ -2,7 +2,8 @@
 from openstack import connection
 
 #create a connection object for the given cloud - devstack (found in clouds.yaml)
-conn = connection.from_config(cloud_name='devstack')
+# so we're creating with user admin
+conn = connection.from_config(cloud_name='devstack-admin')
 
 #list servers and delete all of them (if wanted)
 print("List Servers:")
@@ -29,18 +30,19 @@ networks=[{"uuid": network_uuid}]
 #image id (cirros)
 for image in conn.compute.images():
 	iid = image.id
+
 #create instance 1
 
+# auto creates with the availability zone that the node you're running on is currently a part of 
 instance1 = conn.compute.create_server(name='instance1', flavor_id=fid, image_id=iid)
 instance1 = conn.compute.wait_for_server(instance1)
 
-#currently: THROWS AN ERROR. public network does not seem to work, nor does az
 #create instane 2
-#instance2 = conn.compute.create_server(name='instance2', flavor_id=fid, image_id=iid, availability_zone='az-capstone4', networks=networks)
-#instance2 = conn.compute.wait_for_server(instance)
+#instance2 = conn.compute.create_server(name='instance2', flavor_id=fid, image_id=iid, availability_zone='AZ1', networks=networks)
+#instance2 = conn.compute.wait_for_server(instance2)
 
 print(instance1)
 #print(instance2)
 
-conn.compute.delete_server(instance1)
+#conn.compute.delete_server(instance1)
 #conn.compute.delete_server(instance2)
