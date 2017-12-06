f5626dc0d911e080dd681c4751005c91a338602d
James Taylor <jamestaylr@gmail.com>
2017-12-06 10:06:08 -0500

    Add network ping test with concurrency

--- maxinet-code/perf/switch_experiment.py
+++ maxinet-code/perf/switch_experiment.py
@@ -1,3 +1,5 @@
+import itertools
+import threading
 import time
 import logging
 
@@ -27,3 +29,45 @@ class SwitchExperiment(Experiment):
         for sink, sources in [(v[i], v[i+1:] + v[:i]) for i in range(len(v))]:
             time.sleep(10)
             pass
+
+    def expr_node(self, node):
+        return self.get_node('s{}'.format(node._sid))
+
+    def test_link(self, link):
+        src, dest = link
+        src_node = self._switches[src]
+        dest_node = self._switches[dest]
+        self.expr_node(src_node).cmd('ping -c 5 {}'.format(dest_node._ip_addr))
+        self.expr_node(dest_node).cmd('ping -c 5 {}'.format(src_node._ip_addr))
+
+    def ping_all(self):
+        for src, dest in self.links:
+            src_node = self._switches[src]
+            dest_node = self._switches[dest]
+            logging.info('Testing link {} ({}) <-> {} ({})'.format(
+                src_node._sid,
+                src_node._ip_addr,
+                dest_node._sid,
+                dest_node._ip_addr,
+            ))
+            self.expr_node(src_node).cmd('ping -c 5 {}'.format(dest_node._ip_addr))
+            self.expr_node(dest_node).cmd('ping -c 5 {}'.format(src_node._ip_addr))
+
+        for i in range(0, len(self._switches)):
+            for srcs in itertools.permutations(self._switches.keys(), i):
+                dests = set(self._switches.keys()) - set(srcs)
+                links = [(s, d) for s, d in self.links \
+                        if (s in srcs and d in dests) \
+                            or (d in srcs and s in srcs)]
+                if links == []:
+                    continue
+
+                logging.info('Concurrently testing: {}'.format(links))
+                threads = []
+                for link in links:
+                    t = threading.Thread(target=self.test_link, args=(link,))
+                    t.start()
+                    threads.append(t)
+
+                for t in threads:
+                    t.join()
