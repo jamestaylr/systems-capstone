20c513195d264427f1a2e302f16bc5a036df04b0
Christy Coghlan <ccogs@vt.edu>
2017-12-06 19:24:47 -0500

    Beginning of the summary branch

--- src/helpers.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-def bootImage(azName, imageName):
-        subprocess.call(["openstack", "server", "create", "--flavor m1.tiny", "--image cirros-0.3.5-x86_64-disk", "--nic net-name=public", "--availability-zone " + azName, imageName])
--- src/run_forever.sh
+++ src/run_forever.sh
@@ -2,4 +2,6 @@
 Used for benchmarking nodes.
 Run using nohup via ssh.
 """
-while true; do ./stress; sleep 2; done
+while true; 
+do ./stress -s 20 -M 256 -m 2; 
+sleep 2; done
