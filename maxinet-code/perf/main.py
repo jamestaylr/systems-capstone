a8b7bb03611f54855583f83ba3b2a52a8db5a76c
maxinet <maxinet@worker1>
2017-12-09 11:55:39 -0600

    added command line arg to determine topology

--- maxinet-code/perf/main.py
+++ maxinet-code/perf/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 import logging
+import sys
 from switch_node import SwitchNode
 from switch_experiment import SwitchExperiment
 from MaxiNet.Frontend.maxinet import Cluster
@@ -11,12 +12,50 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 # Create the experiment
-switches = {
-    1: SwitchNode(1, 0, [3]),
-    2: SwitchNode(2, 0, [3]),
-    3: SwitchNode(3, 1, [1, 2, 4, 5]),
-    4: SwitchNode(4, 1, [3]),
-    5: SwitchNode(5, 1, [3]),
-}
-expr = SwitchExperiment(Cluster(), Topo(), switches, switch=OVSSwitch)
-expr.ping_all()
+if (len(sys.argv) < 2):
+	print "Name the topology: fork, star, mesh, or tree"
+else:
+	if (sys.argv[1] == "star"):
+		switches = {
+    			1: SwitchNode(1, 0, [3]),
+    			2: SwitchNode(2, 0, [3]),
+    			3: SwitchNode(3, 0, [1, 2, 4, 5, 6, 7]),
+    			4: SwitchNode(4, 1, [3]),
+    			5: SwitchNode(5, 1, [3]),
+			6: SwitchNode(6, 1, [3]),
+			7: SwitchNode(7, 1, [3]),
+		}
+	elif (sys.argv[1] == "mesh"):
+                switches = {
+                        1: SwitchNode(1, 0, [2, 3, 4, 5, 6, 7]),
+                        2: SwitchNode(2, 0, [1, 3, 4, 5, 6, 7]),
+                        3: SwitchNode(3, 0, [1, 2, 4, 5, 6, 7]),
+                        4: SwitchNode(4, 1, [1, 2, 3, 5, 6, 7]),
+                        5: SwitchNode(5, 1, [1, 2, 3, 4, 6, 7]),
+			6: SwitchNode(6, 1, [1, 2, 3, 4, 5, 7]),
+			7: SwitchNode(7, 1, [1, 2, 3, 4, 5, 6]),
+                }
+	elif (sys.argv[1] == "fork"):
+                switches = {
+                        1: SwitchNode(1, 0, [2, 3]),
+                        2: SwitchNode(2, 0, [1]),
+                        3: SwitchNode(3, 0, [1, 4, 6]),
+                        4: SwitchNode(4, 1, [3, 5]),
+                        5: SwitchNode(5, 1, [4]),
+			6: SwitchNode(6, 1, [3, 7]),
+			7: SwitchNode(7, 1, [6])
+                }
+
+	else: #assume tree
+                switches = {
+                        1: SwitchNode(1, 0, [3, 4, 5]),
+                        2: SwitchNode(2, 0, [3, 6, 7]),
+                        3: SwitchNode(3, 0, [1, 2]),
+                        4: SwitchNode(4, 1, [1]),
+                        5: SwitchNode(5, 1, [1]),
+			6: SwitchNode(6, 1, [2]),
+			7: SwitchNode(7, 1, [2])
+                }
+		
+	expr = SwitchExperiment(Cluster(), Topo(), switches, switch=OVSSwitch)
+	expr.ping_all()
